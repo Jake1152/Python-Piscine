cd5e233ecc55d100ebe2aeb15e585cde507ea976
Jake1152 <rancho974@gmail.com>
2025-09-03 18:38:42 +0900

    chore: ex02 tester와 분리

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,6 +1,3 @@
-from time import time
-from datetime import datetime
-
 def all_thing_is_obj(object: any) -> int:
     type_str = ''
     if isinstance(object, list):
@@ -18,36 +15,3 @@ def all_thing_is_obj(object: any) -> int:
         return 42
     print(f"{type_str} : {type(object)}")
     return 42
-
-def main():
-    ''' 출력문 예시
-    $>python tester.py | cat -e
-    List : <class 'list'>$
-    Tuple : <class 'tuple'>$
-    Set : <class 'set'>$
-    Dict : <class 'dict'>$
-    Brian is in the kitchen : <class 'str'>$
-    Toto is in the kitchen : <class 'str'>$
-    Type not found$
-    42$
-    $>
-    '''
-    from find_ft_type import all_thing_is_obj
-
-    ft_list = ["Hello", "tata!"]
-    ft_tuple = ("Hello", "toto!")
-    ft_set = {"Hello", "tutu!"}
-    ft_dict = {"Hello" : "titi!"}
-
-    all_thing_is_obj(ft_list)
-    all_thing_is_obj(ft_tuple)
-    all_thing_is_obj(ft_set)
-    all_thing_is_obj(ft_dict)
-    all_thing_is_obj("Brian")
-    all_thing_is_obj("Toto")
-
-    print(all_thing_is_obj(10))
-
-
-if __name__ == "__main__":
-    main()
