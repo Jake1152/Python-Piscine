a843215ec31a56465613812d593e506c34d85a69
Jake1152 <rancho974@gmail.com>
2025-09-05 16:35:04 +0900

    feat: ex06, fitler

--- starting_0/ex06/filterstring.py
+++ starting_0/ex06/filterstring.py
@@ -1,12 +1,14 @@
-'''
-# flake8사용
+"""
+# flake8사용.
+
 pip install flake8
 export PATH="/home/jim/.local/bin:$PATH"
-'''
-'''
-$>python building.py "Python 3.0, released in 2008, was a major revision 
-    that is not completely backward
-compatible with earlier versions. 
+"""
+
+"""
+$>python building.py "Python 3.0, released in 2008, was a major revision
+that is not completely backward
+compatible with earlier versions.
 Python 2 was discontinued with version 2.7.18 in 2020."
 The text contains 171 characters:
 2 upper letters
@@ -15,85 +17,54 @@ The text contains 171 characters:
 26 spaces
 15 digits
 $>
-'''
-from sys import argv, stdin
-
-def building(corpus):
-    """
-    문자열을 넘겨받아서 각 조건에 맞게 카운팅
-    한 글자씩 읽어서 처리
-    """
-    def ispunctuation(ch):
-        """
-        문장부호 검사용
-        """
-        import string 
-        if ch in string.punctuation:
-            return True
-        return False
-
-    count_dict = {"lower": 0, "upper":0, "punctuation": 0, "space": 0, "digit": 0 } # default dict사용하면 간단
-    for ch in corpus:
-        if ch.islower():
-            count_dict["lower"] += 1
-        elif ch.isupper():
-            count_dict["upper"] += 1
-        elif ispunctuation(ch):
-            count_dict["punctuation"] += 1
-        elif ch.isspace():
-            count_dict["space"] += 1
-        elif ch.isdigit():
-            count_dict["digit"] += 1
-        # else: # 엄격하게 처리하는 버젼
-        #     raise ValueError(f"유효하지 않은 데이터가 있습니다. 문제의 문자: {ch}")
-
-    total_ch_count =  count_dict["upper"] + count_dict["lower"] + count_dict["punctuation"] + count_dict["space"] + count_dict["digit"]
-    print(f"The text contains {total_ch_count} characters:")
-    print(f"{count_dict["upper"]} upper letters")
-    print(f"{count_dict["lower"]} lower letters")
-    print(f"{count_dict["punctuation"]} punctuation marks")
-    print(f"{count_dict["space"]} spaces")
-    print(f"{count_dict["digit"]} digits")
-    return 0
+"""
 
 
-def get_corpus(args):
-    """
-    말뭉치 전달 및 인자 검증
-    """
+def parse_argument(args):
+    """validation과 parse를 같이 진행한다."""
     corpus = ""
-    if len(args) > 2:
-        raise ValueError("more than one argument is provided")
-    elif len(args) < 2:
-        try:
-            print("What is the text to count?")
-            # while 
-            corpus = stdin.readline()
-        except Exception as err:
-            raise(err)
+    if len(args) != 3:
+        raise ValueError("the arguments are bad")
+    try:
+        num = int(args[2])
+        if num < 0:
+            raise ValueError("err")
+    except ValueError:
+        raise ValueError("the arguments are bad")
+
+    if (not isinstance(args[1], str)) or (not isinstance(num, int)):
+        raise ValueError("the arguments are bad")
     else:
         corpus = args[1]
-    return corpus
+    return [corpus, num]
+
 
 def handle_error(err):
-    """
-    에러핸들링
-    """
+    """에러핸들링."""
     print(f"AssertionError : {err}")
     return 1
 
+
 def main():
     """
-    프로그램 실행 흐름만 담당함
-    인자받아서 처리함수로 넘기고 결과 받기까지 등등.
+    프로그램 실행 흐름만 담당함.
+
+    인자받아서 처리함수로 넘기고 결과 받기까지 등등
     """
+    from sys import argv
+    from ft_filter import ft_filter
+
     try:
         args = argv
-        corpus = get_corpus(args)
-        building(corpus)
+        corpus, num = parse_argument(args)
+        words = corpus.split()
+        filtered_iterable = ft_filter(lambda x: len(x) > num, words)
+        if filtered_iterable != 1:
+            print(filtered_iterable)
 
     except ValueError as err:
         return handle_error(err)
+
 
 if __name__ == "__main__":
     main()
---
+++ starting_0/ex06/ft_filter.py
@@ -0,0 +1,51 @@
+"""
+# flake8사용.
+
+pip install flake8
+export PATH="/home/jim/.local/bin:$PATH"
+alias norminette="flake8"
+"""
+
+
+"""
+filter(조건거르는 함수, 반복가능한 자료구조).
+
+e.g) for _ in fitler(isdigit, [1,'a','b', 42])
+iterable data는 dict, set, tuple 등도 되는가?
+list만 되는가?
+=> list comprehension을 쓰라고 했으므로 list로만 구현한다.
+어떻게 iterable한지 알아낼 것인가?
+직접 dict, set, tuple, list인지 비교해서?
+이미 있는 메서드는 없는가?  is_iterable
+아래 유형별로 별도처리
+- set, dict는 합쳐도 되는지 고려
+- list, tuple도 분리안해도 되는지도 조사
+- list, tuple
+- dict
+- set
+"""
+
+
+def handle_error(err):
+    """에러핸들링."""
+    print(f"AssertionError : {err}")
+    return 1
+
+
+def ft_filter(function, iterable):
+    """
+    Note.
+
+    that filter(function, iterable) is equivalent to the generator expression
+    (item for item in iterable if function(item)) if function is not None
+    and (item for item in iterable if item) if function is None.
+    """
+    try:
+        callable(function)
+        iter(iterable)
+    except ValueError:
+        msg = "Please be adjust argument for ft_filer(function, iterable)"
+        raise ValueError(msg)
+
+    filtered_iterable = [val for val in iterable if function(val)]
+    return filtered_iterable
